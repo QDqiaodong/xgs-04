09c4b131d0019530532fc3a8b5cc4a05bf2792c5
QDqiaodong <573717339@qq.com>
2026-06-12 19:21:47 +0800

    其他模型修复编译问题

--- backend/src/main/java/com/bookexchange/repository/NotificationRepository.java
+++ backend/src/main/java/com/bookexchange/repository/NotificationRepository.java
@@ -2,6 +2,7 @@ package com.bookexchange.repository;
 
 import com.bookexchange.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
+public interface NotificationRepository extends JpaRepository<Notification, Long>, JpaSpecificationExecutor<Notification> {
 
     List<Notification> findByUserIdOrderByCreateTimeDesc(Long userId);
 
